617b0fc5ff0d0fcc2d43177a2b1cc09872ee42a0
neoma287 <79696395+neoma287@users.noreply.github.com>
2022-03-09 18:50:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX=0;
+noseY=0;
+difference =0;
+rightWristX =0;
+leftWristX=0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size = (550 , 500);
+    canvas = createCanvas(550 , 550);
+    canvas.position(560 , 150);
+    poseNet = ml5.poseNet(video , modelLoaded );
+    poseNet.on ('pose',gotPoses);
+}
+function draw () {
+    background('#969A97');
+    fill ('#d193f5');
+    stroke ('#d193f5');
+    square(noseX,noseY,difference);
+
+}
+function modelLoaded(){
+    console.log ('Posenet is initialized!');
+}
+function gotPoses(results){
+    if (results.length >0 )
+    {
+        console.log (results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX =" +noseX +"noseY =" + noseY );
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[o].pose.rightWristX.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX =" + leftWristX+"rightWristX = " + rightWristX + "difference =" + difference);
+    }
+}
